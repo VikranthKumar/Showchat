63db5e40fd73446cd7aae319c70b29d1af8773f7
Vikranth Kumar <srisnevisa@gmail.com>
2023-11-25 20:41:05 -0500

    Initial Commit

---
+++ Showchat/ShowchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowchatApp.swift
+//  Showchat
+//
+//  Created by Vikranth Kumar on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ShowchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
